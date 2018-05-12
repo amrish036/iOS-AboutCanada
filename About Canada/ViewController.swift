fbc1e3cb38c606d824bd50f29a94408b4b9cc527
amrish036 <amrish.036@gmail.com>
2018-05-12 12:46:36 +1000

    Initial Commit

---
+++ About Canada/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  About Canada
+//
+//  Created by Amrish Mahesh on 12/5/18.
+//  Copyright © 2018 Amrish Mahesh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
