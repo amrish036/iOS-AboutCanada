babf0f0f6530897bfe5c5c98429b77d6c68a4322
amrish036 <amrish.036@gmail.com>
2018-05-12 13:22:33 +1000

    Include Alamofire and SwiftyJSON and request data from url

--- About Canada/ViewController.swift
+++ About Canada/ViewController.swift
@@ -7,19 +7,41 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
-
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    getJSONData()
   }
-
+  
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
-
-
+  
+  // MARK: Network Calls
+  func getJSONData(){
+    
+    let requestURL = "https://dl.dropboxusercontent.com/s/2iodh4vg0eortkl/facts.json/get"
+    
+    Alamofire.request(requestURL).responseString { response in
+      
+      if let responseObject = response.result.value {
+        print(responseObject)
+        if let dataFromString = responseObject.data(using: .utf8, allowLossyConversion: false) {
+          let json = try! JSON(data: dataFromString)
+          print(json)
+        }
+      }else{
+        print("Error Loading Content")
+      }
+    }
+  }
+  
+  
 }
 
