c82ace9f3893a1c826e2b9a60762153b9d57e555
amrish036 <amrish.036@gmail.com>
2018-05-12 18:10:28 +1000

    Create Collection view and Load image from url

---
+++ About Canada/CustomCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CustomCollectionViewCell.swift
+//  About Canada
+//
+//  Created by Amrish Mahesh on 12/5/18.
+//  Copyright © 2018 Amrish Mahesh. All rights reserved.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+    
+  @IBOutlet weak var imageCell: UIImageView!
+  @IBOutlet weak var labelCell: UILabel!
+  
+}
+
--- About Canada/ViewController.swift
+++ About Canada/ViewController.swift
@@ -10,12 +10,59 @@ import UIKit
 import Alamofire
 import SwiftyJSON
 
-class ViewController: UIViewController {
+struct Element {
+  let title: String?
+  let description: String?
+  let imageHref: String?
+}
+
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+  
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return self.elements.count
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cell = CollectionView.dequeueReusableCell(withReuseIdentifier: "customCell", for: indexPath) as! CustomCollectionViewCell
+    
+    if self.elements.count > 0  {
+      
+      if(self.elements[indexPath.row].imageHref != nil){
+        let imageUrl: URL? = URL(string: self.elements[indexPath.row].imageHref!)
+        let data = try? Data(contentsOf: imageUrl!)
+        
+        if let imageData = data {
+          cell.imageCell.image = UIImage(data: imageData)
+        }
+      }else{
+        cell.imageCell.image = UIImage(named: "default-image.png")
+      }
+      
+      
+      cell.labelCell.text = self.elements[indexPath.row].title
+      
+      return cell
+      
+    }
+    
+    cell.imageCell.image = UIImage(named: "default-image.png")
+    cell.labelCell.text = "Canada"
+    
+    return cell
+  }
+  
+  var elements = [Element]()
+  
+  @IBOutlet weak var CollectionView: UICollectionView!
+  
   
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    CollectionView.delegate = self
+    CollectionView.dataSource = self
     getJSONData()
+    
   }
   
   override func didReceiveMemoryWarning() {
@@ -35,10 +82,19 @@ class ViewController: UIViewController {
         if let dataFromString = responseObject.data(using: .utf8, allowLossyConversion: false) {
           let json = try! JSON(data: dataFromString)
           print(json)
+          let allElements = json["rows"]
+          
+          for item in allElements {
+            let thisItem = Element(title:item.1["title"].string,
+                                   description:item.1["description"].string,
+                                   imageHref:(item.1["imageHref"].string))
+            self.elements.append(thisItem)
+          }
         }
       }else{
         print("Error Loading Content")
       }
+      self.CollectionView.reloadData()
     }
   }
   
